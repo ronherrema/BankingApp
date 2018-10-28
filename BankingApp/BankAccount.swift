eb330dac8f22699e24ee2fa81e89f4537364adcd
Ron Herrema <rherrema@gmail.com>
2018-10-28 19:11:13 +0000

    First commit of basic banking app.

---
+++ BankingApp/BankAccount.swift
@@ -0,0 +1,24 @@
+//
+//  BankAccount.swift
+//  BankingApp
+//
+//  Created by R J Herrema on 28/10/2018.
+//  Copyright © 2018 R J Herrema. All rights reserved.
+//
+
+import Foundation
+
+class BankAccount {
+    var balance: Double = 0
+    
+    
+    func deposit(pounds: Double) {
+        balance += pounds
+    }
+    
+    func withdraw(pounds: Double) {
+        balance += pounds
+    }
+    
+    
+}
--- BankingApp/ViewController.swift
+++ BankingApp/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    var account = BankAccount()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.enterAmount.delegate = self
+        
     }
 
-
+    
+    @IBOutlet weak var enterAmount: UITextField!
+    
+    @IBOutlet weak var displayBalance: UILabel!
+    
+    @IBAction func enterDeposit(_ sender: UIButton) {
+        let doubleFromString = Double(enterAmount.text!)
+        account.balance += doubleFromString!
+        displayBalance.text = "£" + String(format: "%.2f", account.balance)
+    }
+    
+    @IBAction func withdrawMoney(_ sender: UIButton) {
+        let doubleFromString = Double(enterAmount.text!)
+        account.balance -= doubleFromString!
+        displayBalance.text = "£" + String(format: "%.2f", account.balance)
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        textField.resignFirstResponder()
+        return false
+    }
 }
 
